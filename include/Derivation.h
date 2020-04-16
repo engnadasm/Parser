3eb3c0ae4ab8789ae3f4f6c69e82ca76bc8b4920
engnadasm <42079713+engnadasm@users.noreply.github.com>
2020-04-16 22:17:51 +0200

    add Derivation.h

---
+++ include/Derivation.h
@@ -0,0 +1,39 @@
+#ifndef DERIVATION_H
+#define DERIVATION_H
+#include <iostream>
+#include <queue>          // std::queue
+#include <stack> // std::stack
+#include<set>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include "../include/Rule.h"
+using namespace std;
+
+class Derivation
+{
+
+    public:
+        static Derivation* GetInstance();
+        void generateDerivations();
+        void PrintDerivations();
+        void enterToken(string token);
+        void setParsingTable(unordered_map<string,unordered_map<string, vector<Rule*>>> parsingTable);
+        void setStart(Rule* start);
+        bool getAccept();
+        void endDerivation();
+
+    protected:
+
+    private:
+        Derivation();
+        static Derivation* derivationInstance;		// singleton instance
+        queue<string> tokens;
+        stack<Rule*> ourStack;
+        vector<string> outToPrint;
+        int currentPointer = 0;
+        unordered_map<string,unordered_map<string, vector<Rule*>>> ourParsingTable;
+        bool accept = true;
+};
+
+#endif // DERIVATION_H
